d7ec286941a61b4640900829b50650173788b221
Mireiden <miroslavucetic@gmail.com>
2025-12-27 20:40:31 +0100

    Normalizovanje dimenzija: Dodat dpi awareness za Windows Velicina prozora i table se racunaju prema velicini ekrana.

--- src/main.py
+++ src/main.py
@@ -1,21 +1,42 @@
+import math
+
 from src.models.igra import Igra
 import pygame
 from src.models.tabla import Tabla
 from src.gui.renderer import nacrtaj_tablu
 from src.gui.input import nadji_kliknuto_polje
+import ctypes
+import platform
 
 if __name__ == "__main__":
+    # Postavljanje dpi awareness
+    if platform.system() == "Windows":
+        try:
+            # Windows 8.1 i noviji
+            ctypes.windll.shcore.SetProcessDpiAwareness(1)
+        except:
+            try:
+                # Windows Vista i noviji
+                ctypes.windll.user32.SetProcessDPIAware()
+            except:
+                pass
 
     igra = Igra.konstrukcija()
     igra.tabla.prikaz_polja()
 
     pygame.init()
-    screen = pygame.display.set_mode((1600, 1200))
-    pygame.display.set_caption("Atoll")
+
+    info = pygame.display.Info()
+    screen_width = int(info.current_w * 0.9)
+    screen_height = int(info.current_h * 0.80)
+    screen = pygame.display.set_mode((screen_width, screen_height))
 
     tabla = igra.tabla
-    HEX_SIZE = 30
-
+    # HEX_SIZE = 30
+    #sirina polja = 2 * HEX_SIZE
+    #visina polja = √3 × HEX_SIZE
+    max_board_height = screen_height * 0.9
+    HEX_SIZE = max_board_height / ((2 * tabla.n + 1) * math.sqrt(3))
     running = True
     offset_x, offset_y = 0, 0
 
